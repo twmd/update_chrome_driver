3414e303c1607dd6a85cac633fb4c188dbb2f83d
Roman Chutchev <RChutchev@gmail.com>
2022-06-01 03:06:45 +0300

    V2
    Актуализация до актуального состояния из репо twmd и исправление ошибок
    
    Co-Authored-By: twmd <4730968+twmd@users.noreply.github.com>

--- ucd.py
+++ ucd.py
@@ -6,13 +6,24 @@ from urllib import request
 import zipfile
 import sys
 import configparser
-import ast
+import logging
+import logging.config
 import shutil
 import time
 
-#TODO: Добавить логирование
 SITE_URL = 'https://chromedriver.chromium.org/downloads'
 DOWNLOAD_FOLDER = os.path.join(os.getcwd(), 'download')
+
+
+def get_logger():
+    # Логер
+    logging.config.fileConfig(os.path.join(os.getcwd(), 'logger.ini'), disable_existing_loggers=False)
+    logger = logging.getLogger('plogger')
+    return logger
+
+
+logger = get_logger()
+
 
 def get_config(config_path: str, section: str) -> dict:
     """
@@ -23,39 +34,41 @@ def get_config(config_path: str, section: str) -> dict:
     """
     config = configparser.ConfigParser()
     config.read(config_path)
+    logger.info('Фаил конфигурации прочитан')
     return dict(config.items(section))
 
 
 def get_current_drivers(folder: str) -> list:
-    '''
+    """
     Ищет файлы драйвера в указанной директории
     :param folder: Путь к директории с драйверами
     :return: версий драйверов
-    '''
+    """
     driver_ver = []
     re_dig = re.compile(r"\d+")
-    if os.path.exists(DRIVER_FOLDER):
-        for file in os.listdir(DRIVER_FOLDER):
+    if os.path.exists(folder):
+        for file in os.listdir(folder):
             if file.startswith('chromedriver'):
                 ver = re.search(re_dig, file)
                 if ver:
                     driver_ver.append(ver.group(0))
     else:
-        print('Тут будет exc')
+        logger.error('Каталога с драйверами не существует')
     return driver_ver
 
 
 def get_new_drivers_list(url: str, driver_ver: list) -> list:
-    '''
+    """
     Парсим сайт с загрузками выбираем ссылки на страницу загрузки
     :param url: URL
     :return: список с сайтами для загрузки
-    '''
+    """
+    logger.info('get_new_drivers_list запустился')
     url_list = []
     try:
         page = requests.get(url)
     except Exception as e:
-        print(e)
+        logger.exception('Ошибка подключения к сайту')
         sys.exit(1)
     soup = bs(page.text, features='html.parser')
     search_string = 'chromedriver.storage.googleapis.com'
@@ -67,15 +80,18 @@ def get_new_drivers_list(url: str, driver_ver: list) -> list:
             ver_url = link.split('=')[-1].split('.')[0]
             if ver_url not in driver_ver:
                 url_list.append(link)
+    logger.info('get_new_drivers_list выполнился')
     return url_list
 
 
 def download_drivers(url_list, download_folder):
-    '''
+    """
     Загрузка драйверов
+    :param download_folder:
     :param url_list:
     :return:
-    '''
+    """
+    logger.info('download_drivers запустился')
     driver_dict = {}
     driver_name = r'chromedriver_win32.zip'
     # Формируем словарь версия драйвер:адресс для скачивания
@@ -87,43 +103,64 @@ def download_drivers(url_list, download_folder):
     # Сохраняем фаил
     for ver, url in driver_dict.items():
         tmp_file_name = ver + '_' + url.split('/')[-1]
-        #Задаем имя файла и катало гля загрузки в формате ver_...
+        # Задаем имя файла и катало гля загрузки в формате ver_...
         if not os.path.exists(download_folder):
             os.makedirs(download_folder)
         zip_file_name = os.path.join(download_folder, tmp_file_name)
         try:
             request.urlretrieve(url, zip_file_name)
         except Exception as e:
-            print(e)
+            logger.exception('Ошибка загрузки драйверов')
+    logger.info('download_drivers завершил работу')
+
 
 def uzip_rename_move(src_folder, dst_folder):
-    '''
+    """
     Распаковывает архив, и переименовывает фаил с перемещением
     :param src_folder: Директория для загрузки
     :param dst_folder: Директория куда складывать драйвера
     :return:
-    '''
-    #Проверяем файлы в каталоге загрузки, ищем .zip
+    """
+    logger.info('uzip_rename_move запустился')
+    # Проверяем файлы в каталоге загрузки, ищем .zip
     for file in os.listdir(src_folder):
         if file.endswith('.zip'):
-            #Получаем версию
+            # Получаем версию
             ver = file.split('_')[0]
-            #Формируем имя файла и полный путь
+            # Формируем имя файла и полный путь
             driver_name = 'chromedriver' + ver + '.exe'
             full_path = os.path.join(src_folder, file)
             with zipfile.ZipFile(full_path, 'r') as zf:
                 zf.extractall(DOWNLOAD_FOLDER)
             time.sleep(2)
             os.remove(full_path)
+            # Create destination folder if not exists
+            if not os.path.exists(dst_folder):
+                try:
+                    os.makedirs(dst_folder)
+                    logger.info('Папка назначения была создана по пути указанному в конфиге')
+                except OSError as e:
+                    logger.exception('Ошибка создания папки назначения по пути указанному в конфиге.')
+                    try:
+                        shutil.rmtree(src_folder)
+                        logger.info('Папка с новыми драйверами удалена')
+                    except Exception as e:
+                        logger.exception('Папка с новыми драйверами не может быть удалена')
+                    logger.info('uzip_rename_move завершил работу')
+                    logger.info('Драйверы НЕ обновлены')
+                    sys.exit(1)
             for file_exe in os.listdir(src_folder):
                 if file_exe.endswith('exe'):
                     try:
-                        shutil.move(os.path.join(src_folder, file_exe), os.path.join(dst_folder, driver_name))
                         # os.rename(os.path.join(src_folder, file_exe), os.path.join(dst_folder, driver_name))
+                        shutil.move(os.path.join(src_folder, file_exe), os.path.join(dst_folder, driver_name))
                     except Exception as e:
-                        print(e)
+                        logger.exception('Ошибка перемещения файлов')
+    logger.info('uzip_rename_move завершил работу')
+
 
 if __name__ == '__main__':
+    logger.info('Скрипт начал работу')
     config = get_config(os.path.join(os.getcwd(), 'setting.ini'), 'FOLDER')
     DRIVER_FOLDER = config['driver_folder']
     driver_ver_list = get_current_drivers(DRIVER_FOLDER)
@@ -131,3 +168,6 @@ if __name__ == '__main__':
     if url_list:
         download_drivers(url_list, DOWNLOAD_FOLDER)
         uzip_rename_move(DOWNLOAD_FOLDER, DRIVER_FOLDER)
+        logger.info('Драйверы обновлены')
+    else:
+        logger.info('Обновлений драйверов не было')
